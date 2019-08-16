0682b19cc7da6bfcbb3c1b53ed434a85394735db
Vincent Calabro <vinmann@gmail.com>
2019-08-16 14:50:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,15 +24,9 @@
       mounted () {
         this.$refs.signinBtn.addEventListener('click', e => {
 
-          window.FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-              this.$emit('success')
-            } else {
-              window.FB.login(response => {
-                this.$emit(response.authResponse ? 'success' : 'error', response)
-              }, this.params);
-            }
-          });
+          window.FB.login(response => {
+            this.$emit(response.authResponse ? 'success' : 'error', response)
+          }, this.params)
 
         })
       }
